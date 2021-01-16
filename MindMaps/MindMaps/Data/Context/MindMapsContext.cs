e0ca17afc1a945ab223a42557c4114d62c9907da
marijadstankovic <marija.d.stankovic@elfak.rs>
2021-01-16 12:24:43 +0100

    Initiate data layer

---
+++ MindMaps/MindMaps/Data/Context/MindMapsContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MindMaps.Data.Context
+{
+    public class MindMapsContext : DbContext
+    {
+        public MindMapsContext()
+        {
+        }
+    }
+}
