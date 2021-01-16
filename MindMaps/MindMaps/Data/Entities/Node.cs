717d2b54086d59ed6f182c06e234321ce1ce8bc7
marijadstankovic <marija.d.stankovic@elfak.rs>
2021-01-16 13:08:09 +0100

    Write class Node

--- MindMaps/MindMaps/Data/Entities/Node.cs
+++ MindMaps/MindMaps/Data/Entities/Node.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace MindMaps.Data.Entities
 {
     public class Node
     {
+        public int ID { get; set; }
+
+        public string XMLText { get; set; }
+
+        public int MindMapID { get; set; }
+
+        public MindMap MindMap { get; set; }
+
+        public int XMLID { get; set; }
+
+        public int UserID { get; set; }
+
+        public User User { get; set; }
     }
 }
