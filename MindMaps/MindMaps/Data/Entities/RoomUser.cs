4e45423696cf75b2e65f11e12564a957c6167420
marijadstankovic <marija.d.stankovic@elfak.rs>
2021-01-16 13:00:04 +0100

    Write classes Room, RoomUser, Users

--- MindMaps/MindMaps/Data/Entities/RoomUser.cs
+++ MindMaps/MindMaps/Data/Entities/RoomUser.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace MindMaps.Data.Entities
 {
     public class RoomUser
     {
+        public int ID { get; set; }
+
+        public int RoomID { get; set; }
+
+        public Room Room { get; set; }
+
+        public int UserID { get; set; }
+
+        public User User { get; set; }
     }
 }
--- MindMaps/MindMaps/Data/Entities/User.cs
+++ MindMaps/MindMaps/Data/Entities/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
@@ -8,5 +9,25 @@ namespace MindMaps.Data.Entities
 {
     public class User
     {
+        public int ID { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [MaxLength(100)]
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public ICollection<RoomUser> RoomUsers { get; set; }
+
+        public ICollection<Message> Messages { get; set; }
+
+        public ICollection<Node> Nodes { get; set; }
+
+        public ICollection<Comment> Comments { get; set; }
+    
     }
 }
