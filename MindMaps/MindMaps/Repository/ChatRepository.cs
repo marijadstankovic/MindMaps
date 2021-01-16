1b30f5ab063ed110d56bcb9c7b6f055151de6d23
marijadstankovic <marija.d.stankovic@elfak.rs>
2021-01-16 16:10:53 +0100

    Add repositories for entities MindMaps, Message, Comment, Chat

---
+++ MindMaps/MindMaps/Repository/ChatRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MindMaps.Data.Entities;
+using MindMaps.Data.Context;
+
+namespace MindMaps.Repository
+{
+    public class ChatRepository : EfCoreRepository<Chat>
+    {
+        public ChatRepository(MindMapsContext context) : base(context)
+        {
+
+        }
+    }
+}
---
+++ MindMaps/MindMaps/Repository/MindMapRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MindMaps.Data.Entities;
+using MindMaps.Data.Context;
+
+namespace MindMaps.Repository
+{
+    public class MindMapRepository : EfCoreRepository<MindMap>
+    {
+        public MindMapRepository(MindMapsContext context) : base(context)
+        {
+        }
+    }
+}
