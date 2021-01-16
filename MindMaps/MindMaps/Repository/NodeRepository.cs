272f011bc0180cd8125a9bba3cb8087985f513f2
Jelena-Zivkovic <jelena.zivkovic@elfak.rs>
2021-01-16 16:09:56 +0100

    Create NodeRepository, RoomRepository, RoomUserRepository, UserRepository

---
+++ MindMaps/MindMaps/Repository/NodeRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MindMaps.Data.Context;
+using MindMaps.Data.Entities;
+
+namespace MindMaps.Repository
+{
+    public class NodeRepository : EfCoreRepository<Node>
+    {
+        public NodeRepository(MindMapsContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ MindMaps/MindMaps/Repository/RoomUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MindMaps.Data.Context;
+using MindMaps.Data.Entities;
+
+namespace MindMaps.Repository
+{
+    public class RoomUserRepository : EfCoreRepository<RoomUser>
+    {
+        public RoomUserRepository(MindMapsContext context) : base(context)
+        {
+        }
+    }
+}
