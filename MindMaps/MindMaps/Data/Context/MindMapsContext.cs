435b8e07607ff410bb3e486662cfebf39d1ac75b
marijadstankovic <marija.d.stankovic@elfak.rs>
2021-01-16 13:17:47 +0100

    Write context

--- MindMaps/MindMaps/Data/Context/MindMapsContext.cs
+++ MindMaps/MindMaps/Data/Context/MindMapsContext.cs
@@ -3,12 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using MindMaps.Data.Entities;
+
 
 namespace MindMaps.Data.Context
 {
     public class MindMapsContext : DbContext
     {
-        public MindMapsContext()
+        public DbSet<User> User { get; set; }
+        public DbSet<RoomUser> RoomUsers { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Chat> Chats { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<MindMap> MindMaps { get; set; }
+        public DbSet<Node> Nodes { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+        public MindMapsContext(DbContextOptions<MindMapsContext> options) : base(options)
         {
         }
     }
