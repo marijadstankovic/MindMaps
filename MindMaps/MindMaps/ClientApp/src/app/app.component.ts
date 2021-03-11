faaeb4edaefde2e7181b2e15a37e6f9c568bae57
Jelena-Zivkovic <jelena.zivkovic@elfak.rs>
2021-03-11 19:23:23 +0100

    Install mxgraph

--- MindMaps/MindMaps/ClientApp/src/app/app.component.ts
+++ MindMaps/MindMaps/ClientApp/src/app/app.component.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import { AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { mxEvent, mxGraph } from 'mxgraph';
+declare var mxGraph: any;
+declare var mxHierarchicalLayout: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'app';
+
+
+
+
+
+export class AppComponent implements AfterViewInit {
+  @ViewChild('graphContainer', { static: false }) graphContainer: ElementRef;
+
+  ngAfterViewInit() {
+    mxEvent.disableContextMenu(this.graphContainer.nativeElement);
+    const graph = new mxGraph(this.graphContainer.nativeElement);
+    try {
+
+      const parent = graph.getDefaultParent();
+      graph.getModel().beginUpdate();
+      const vertex1 = graph.insertVertex(parent, '1', 'Vertex 1', 0, 0, 200, 80);
+      const vertex2 = graph.insertVertex(parent, '2', 'Vertex 2', 0, 0, 200, 80);
+      graph.insertEdge(parent, '', '', vertex1, vertex2);
+    } finally {
+      graph.getModel().endUpdate();
+      new mxHierarchicalLayout(graph).execute(graph.getDefaultParent());
+    }
+  }
 }
