529abb22ab9b1f9b3f18a3087c794157349e6e18
Jelena-Zivkovic <jelena.zivkovic@elfak.rs>
2021-03-11 20:32:27 +0100

    Update mxGraph

--- MindMaps/MindMaps/ClientApp/src/app/app.component.ts
+++ MindMaps/MindMaps/ClientApp/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { AfterViewInit, ElementRef, ViewChild } from '@angular/core';
-import { mxEvent, mxGraph } from 'mxgraph';
+//import { mxEvent, mxGraph } from 'mxgraph';
 declare var mxGraph: any;
 declare var mxHierarchicalLayout: any;
 
@@ -17,7 +17,8 @@ export class AppComponent implements AfterViewInit {
   @ViewChild('graphContainer', { static: false }) graphContainer: ElementRef;
 
   ngAfterViewInit() {
-    mxEvent.disableContextMenu(this.graphContainer.nativeElement);
+   // mxEvent.disableContextMenu(this.graphContainer.nativeElement);
+   // var mxLoadResources = false;
     const graph = new mxGraph(this.graphContainer.nativeElement);
     try {
 
