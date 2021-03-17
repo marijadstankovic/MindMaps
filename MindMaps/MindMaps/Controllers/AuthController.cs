a944e0e63f9e0e0323a972966a000c9e8d90ad98
Jelena-Zivkovic <jelena.zivkovic@elfak.rs>
2021-03-17 22:56:51 +0100

    Add login in AuthController.cs

--- MindMaps/MindMaps/Controllers/AuthController.cs
+++ MindMaps/MindMaps/Controllers/AuthController.cs
@@ -5,8 +5,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 using MindMaps.Repository;
+using MindMaps.DTOs;
 using MindMaps.Data.Entities;
+using Microsoft.Extensions.Configuration;
 
 
 namespace MindMaps.Controllers
@@ -17,8 +22,12 @@ namespace MindMaps.Controllers
     {
         private readonly IAuthRepository _repo;
 
-        public AuthController(IAuthRepository repo) {
+        private readonly IConfiguration _config;
+
+        public AuthController(IAuthRepository repo, IConfiguration config) {
             _repo = repo;
+            _config = config;
+
         }
 
         [HttpPost("register")]
@@ -36,6 +45,40 @@ namespace MindMaps.Controllers
             var createdUser = await _repo.Register(userToCreate, password);
 
             return StatusCode(201);
+        }
+
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto) {
+            var userFromRepo = await _repo.Login(userForLoginDto.Email, userForLoginDto.Password);
+
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Name)
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new { 
+                token = tokenHandler.WriteToken(token)
+            });
         }
     }
 }
