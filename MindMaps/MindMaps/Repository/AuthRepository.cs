ae888a92c8b6abd07ec7c20a278953b2dc857a2e
marijadstankovic <marija.d.stankovic@elfak.rs>
2021-03-17 21:48:43 +0100

    impl interface

---
+++ MindMaps/MindMaps/Repository/AuthRepository.cs
@@ -0,0 +1,26 @@
+﻿using MindMaps.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MindMaps.Repository
+{
+    public class AuthRepository : IAuthRepository
+    {
+        public Task<User> Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> Register(User user, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UserExists(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
