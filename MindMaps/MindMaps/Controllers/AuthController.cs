104702e39703bc35c7d4f8559c4cf0d3d540a6d1
Jelena-Zivkovic <jelena.zivkovic@elfak.rs>
2021-03-17 22:17:30 +0100

    Add AuthController.cs

---
+++ MindMaps/MindMaps/Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MindMaps.Repository;
+using MindMaps.Data.Entities;
+
+
+namespace MindMaps.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+
+        public AuthController(IAuthRepository repo) {
+            _repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(string username, string password)
+        {
+            username = username.ToLower();
+
+            if (await _repo.UserExists(username))
+                return BadRequest("Username already exists");
+
+            var userToCreate = new User {
+                Email = username
+            };
+
+            var createdUser = await _repo.Register(userToCreate, password);
+
+            return StatusCode(201);
+        }
+    }
+}
