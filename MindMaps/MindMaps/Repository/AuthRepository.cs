25de5d28b9d6a053de903c13209387810486866c
marijadstankovic <marija.d.stankovic@elfak.rs>
2021-03-17 22:02:36 +0100

    implement register method

--- MindMaps/MindMaps/Repository/AuthRepository.cs
+++ MindMaps/MindMaps/Repository/AuthRepository.cs
@@ -1,4 +1,5 @@
-﻿using MindMaps.Data.Entities;
+﻿using MindMaps.Data.Context;
+using MindMaps.Data.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,14 +9,38 @@ namespace MindMaps.Repository
 {
     public class AuthRepository : IAuthRepository
     {
+        private readonly MindMapsContext _context;
+
+        public AuthRepository(MindMapsContext context)
+        {
+            _context = context;
+        }
+
         public Task<User> Login(string username, string password)
         {
             throw new NotImplementedException();
         }
 
-        public Task<User> Register(User user, string password)
+        public async Task<User> Register(User user, string password)
         {
-            throw new NotImplementedException();
+            byte[] passHash, passSalt;
+            CreatePasswordHash(password, out passHash, out passSalt);
+
+            user.PasswordHash = passHash;
+            user.PasswordSalt = passSalt;
+
+            await _context.User.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+        private void CreatePasswordHash(string password, out byte[] passHash, out byte[] passSalt)
+        {
+            using (var hmec = new System.Security.Cryptography.HMACSHA512())
+            {
+                passSalt = hmec.Key;
+                passHash = hmec.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
         }
 
         public Task<bool> UserExists(string username)
